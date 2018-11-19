ac3f2050cdbac76dc7bc31335c30a23db2579422
ho3 <ho3ein.k.t@gmail.com>
2018-11-19 13:19:18 +0330

    timezone

--- mesineapp/models.py
+++ mesineapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
 from datetime import datetime
 from django.utils import timezone
+import pytz
 # from .user_typemodel.py import User_type
 
 class رسانه(models.Model):
@@ -70,8 +71,8 @@ class خبر(models.Model):
     شناسه = models.AutoField(primary_key=True)
     عنوان_خبر=models.CharField(max_length=500)
     شرح_خبر = RichTextUploadingField()
-    تاریخ = models.DateField(default=timezone.now)
-    ساعت = models.TimeField(default=timezone.now)
+    تاریخ = models.DateField(default=timezone.now(pytz.timezone('Asia/Tehran')))
+    ساعت = models.TimeField(default=timezone.now(pytz.timezone('Asia/Tehran')))
     شناسه_عکس = models.ForeignKey(رسانه,on_delete=models.CASCADE, null=True, blank =True ,
     default=None ,max_length = 500 , related_name='nmedia' , help_text = "شناسه رسانه مورد نظر را انتخاب کرده یا رسانه ای جدید ایجاد کنید.")
     شناسه_عکس_مارکتینگ= models.ForeignKey(رسانه,on_delete=models.CASCADE, null=True, blank =True ,
